321303c0f388fc7bbb6b66c54f8c65b8aefb6877
Taichi03 <Taichi03@users.noreply.github.com>
2020-07-23 19:09:51 +0900

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all  # すべてのレコードを@postsに代入
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
   end
 end
